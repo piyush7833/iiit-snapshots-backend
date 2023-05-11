526095e36ede5451071f8d309956b744f46f0fb4
piyush7833 <96736508+piyush7833@users.noreply.github.com>
2023-05-11 18:25:47 +0530

    removing verify token from getting comments

--- Routes/comments.js
+++ Routes/comments.js
@@ -4,8 +4,7 @@ import { verifyToken } from "../verifyToken.js";
 const router=express.Router()
 router.post("/", verifyToken,addComment) 
 router.delete("/:id", verifyToken,deleteComment)
-// router.delete("/:id", verifyToken,deletePhotoComment)
-router.get("/videoComment/:videoId",verifyToken,getVideoComments)
-router.get("/photoComment/:photoId",verifyToken,getPhotoComments)
+router.get("/videoComment/:videoId",getVideoComments)
+router.get("/photoComment/:photoId",getPhotoComments)
 // router.get("/:photoId",getPhotoComments)
 export default router;
