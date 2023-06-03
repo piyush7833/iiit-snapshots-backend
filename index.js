d514058a79d967c3c9be6b585741e504b160f4c8
piyush7833 <96736508+piyush7833@users.noreply.github.com>
2023-06-03 09:28:56 +0530

    everything is right in local

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-import express from "express"
-import mongoose from "mongoose"
-import dotenv from "dotenv"
-import userRoutes from "./Routes/users.js"
-import commentRoutes from "./Routes/comments.js"
-import videoRoutes from "./Routes/videos.js"
-import photoRoutes from "./Routes/photos.js"
-import authRoutes from "./Routes/auth.js"
-import payRoutes from './Routes/pay.js'
-import contactRoutes from './Routes/contacts.js'
-import cookieParser from "cookie-parser"
-import Razorpay from 'razorpay'
-import cors from "cors"
-// const http = require('http');
-// const querystring = require('querystring');
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./Routes/users.js";
+import commentRoutes from "./Routes/comments.js";
+import videoRoutes from "./Routes/videos.js";
+import photoRoutes from "./Routes/photos.js";
+import authRoutes from "./Routes/auth.js";
+import payRoutes from './Routes/pay.js';
+import contactRoutes from './Routes/contacts.js';
+import cookieParser from "cookie-parser";
+import Razorpay from 'razorpay';
+import cors from "cors";
+
 const app = express();
 dotenv.config();
 
@@ -34,36 +33,48 @@ export const instance = new Razorpay({
 });
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://example.com'],
+    origin: true,
     credentials: true
 }));
-app.use(cookieParser())
-app.use(express.json())  //without this our APP will not accept json from any external source
-app.use(express.urlencoded({ extended: true }))  //without this our APP will not accept json from any external source
-app.use("/api/auth", authRoutes)
-app.use("/api/users", userRoutes)
-app.use("/api/videos", videoRoutes)
-app.use("/api/comments", commentRoutes)
-app.use("/api/photos", photoRoutes)
-app.use("/api/pay", payRoutes)
-app.use("/api/contacts", contactRoutes)
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/photos", photoRoutes);
+app.use("/api/pay", payRoutes);
+app.use("/api/contacts", contactRoutes);
 
-app.get("/api/getkey", (req, res, next) => res.status(200).json({ key: process.env.RZP_KEY_ID }))
+app.get("/api/getkey", (req, res, next) => res.status(200).json({ key: process.env.RZP_KEY_ID }));
 
-app.use((err, req, res, next) => {      //for catching error in most useful way   //error.js is created for handling error in customised way without it error handling can be done
+app.use((err, req, res, next) => { 
     const status = err.status || 500;
     const message = err.message || "something went wrong";
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Origin', `http://localhost:3000`);
+    res.setHeader('Access-Control-Allow-Methods', GET, POST, PUT, DELETE, OPTIONS);
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Cookie');
     res.setHeader('Access-Control-Allow-Credentials', true);
-
+    res.setHeader('Access-Control-Expose-Headers', 'Authorization');
+    
     return res.status(status).json({
         success: false,
         status: status,
         message: message
     })
 })
+
+
+// Add the following middleware to handle preflight requests
+// app.options('*', cors({
+//     origin: ['http://localhost:3000', 'https://example.com'],
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+//     credentials: true,
+//     preflightContinue: true
+// }));
+
 app.listen(8800, () => {
     connect();
     console.log("Connected to Server");
