9968eebeddb0781149dfcf5c7a67d6bd833db205
piyush7833 <96736508+piyush7833@users.noreply.github.com>
2023-08-26 16:37:55 +0530

    fixing cors

--- index.js
+++ index.js
@@ -33,10 +33,7 @@ export const instance = new Razorpay({
     key_secret: process.env.RZP_SECRET_KEY,
   });
 
-  app.use(cors({
-    origin: true,
-    credentials: true
-}));
+app.use(cors("*"));
 app.use(cookieParser())
 app.use(express.json())  //without this our APP will not accept json from any external source
 app.use(express.urlencoded({extended :true}))  //without this our APP will not accept json from any external source
