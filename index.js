c901cdd5adf08703140e07457c662bf13f53e1d1
piyush7833 <96736508+piyush7833@users.noreply.github.com>
2023-08-27 11:04:14 +0530

    fixing cors using cgpt

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-import express from "express"
-import mongoose from "mongoose"
-import dotenv from "dotenv"
-import userRoutes from "./Routes/users.js"
-import commentRoutes from "./Routes/comments.js"
-import videoRoutes from "./Routes/videos.js"
-import photoRoutes from "./Routes/photos.js"
-import authRoutes from "./Routes/auth.js"
-import payRoutes from './Routes/pay.js'
-import contactRoutes from './Routes/contacts.js'
-import cookieParser from "cookie-parser"
-import Razorpay from 'razorpay'
-import cors from "cors"
-// const http = require('http');
-// const querystring = require('querystring');
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./Routes/users.js";
+import commentRoutes from "./Routes/comments.js";
+import videoRoutes from "./Routes/videos.js";
+import photoRoutes from "./Routes/photos.js";
+import authRoutes from "./Routes/auth.js";
+import payRoutes from './Routes/pay.js';
+import contactRoutes from './Routes/contacts.js';
+import cookieParser from "cookie-parser";
+import Razorpay from 'razorpay';
+import cors from "cors";
+
 const app = express();
 dotenv.config();
 
@@ -31,32 +30,46 @@ const connect = () => {
 export const instance = new Razorpay({
     key_id: process.env.RZP_KEY_ID,
     key_secret: process.env.RZP_SECRET_KEY,
-  });
+});
 
-app.use(cors("*"));
-app.use(cookieParser())
-app.use(express.json())  //without this our APP will not accept json from any external source
-app.use(express.urlencoded({extended :true}))  //without this our APP will not accept json from any external source
-app.use("/api/auth",authRoutes)
-app.use("/api/users",userRoutes)
-app.use("/api/videos",videoRoutes)
-app.use("/api/comments",commentRoutes)
-app.use("/api/photos",photoRoutes)
-app.use("/api/pay",payRoutes)
-app.use("/api/contacts",contactRoutes)
+const allowedOrigins = ['http://localhost:3000', 'https://iiitusnapshots.netlify.app/']; // Add your frontend URLs
 
-app.get("/api/getkey", (req,res,next)=>res.status(200).json({key:process.env.RZP_KEY_ID}))
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true // Allow cookies to be sent
+};
 
-app.use((err,req,res,next)=>{      //for catching error in most useful way   //error.js is created for handling error in customised way without it error handling can be done
-    const status=err.status || 500;
-    const message=err.message || "something went wrong";
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/photos", photoRoutes);
+app.use("/api/pay", payRoutes);
+app.use("/api/contacts", contactRoutes);
+
+app.get("/api/getkey", (req, res, next) => res.status(200).json({ key: process.env.RZP_KEY_ID }));
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message || "something went wrong";
     return res.status(status).json({
-        success:false,
-        status:status,
-        message:message,
-    })
-})
+        success: false,
+        status: status,
+        message: message,
+    });
+});
+
 app.listen(8800, () => {
     connect();
     console.log("Connected to Server");
-  });
+});
