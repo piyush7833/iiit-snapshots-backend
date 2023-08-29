8835b8c96d6a8c8b42e71f68561049ae2f6fdf1c
piyush7833 <96736508+piyush7833@users.noreply.github.com>
2023-08-29 22:17:17 +0530

    email template

--- emailTemplate.js
+++ emailTemplate.js
@@ -21,6 +21,7 @@ const emailTemplate=(url,subject,userName,para,task,ignore)=>(`
             padding: 20px;
             border: 1px solid #333;
             color: #ffffff;
+            border-radius: 1.3rem;
         }
         .header {
             text-align: center;
@@ -32,24 +33,41 @@ const emailTemplate=(url,subject,userName,para,task,ignore)=>(`
         .content {
             padding: 20px;
         }
+        .btn{
+            display:flex;
+            align-items: center;
+            justify-content: center;
+        }
         .verification-button {
             display: inline-block;
             background-color: #e74c3c;
             color: #ffffff;
             padding: 10px 20px;
             text-decoration: none;
-            border-radius: 5px;
+            border-radius: 0.5rem;
+            transition: all 0.2s;
+        }
+        .verification-button:hover {
+            border-radius: 1.3rem;
         }
         .footer {
+            display: flex;
+            align-items: center;
+            justify-content: center;
             text-align: center;
             background-color: #221d1d;
             padding: 10px;
             color: #ffffff;
+            height: 5vh;
         }
         .footer a {
             color: #ffffff;
             text-decoration: none;
             margin: 0 10px;
+        }
+        .img{
+            height: 3vh;
+            width: 3vh;
         }
     </style>
 </head>
@@ -67,8 +85,8 @@ const emailTemplate=(url,subject,userName,para,task,ignore)=>(`
             <p style="font-style: italic;">Best regards,<br>IIITU Snapshot Team</p>
         </div>
         <div class="footer">
-            <a href="https://www.instagram.com/_piyush_8_3_3/">Instagram</a>
-            <a href="https://www.linkedin.com/in/piyush-singh-403089221/">LinkedIn</a>
+            <a href="https://www.instagram.com/_piyush_8_3_3/"><img src="https://firebasestorage.googleapis.com/v0/b/iiitusnapshots.appspot.com/o/logo%2Finsta.png?alt=media&token=e6213c81-c98e-4c8f-8f6f-3066c4523e17" alt="" class="img"></a>
+            <a href="https://www.linkedin.com/in/piyush-singh-403089221/"><img src="https://firebasestorage.googleapis.com/v0/b/iiitusnapshots.appspot.com/o/logo%2Flinkedin.png?alt=media&token=aef86b70-8d92-45d3-b47e-e50cfd00b2fe" alt="" class="img"></a>
         </div>
     </div>
 </body>
